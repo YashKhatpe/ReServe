ebae28d97f8d90a18725af2af57b8acce889e141
YashKhatpe <yashkhatpe0611@gmail.com>
2025-03-02 09:20:07 +0530

    supabase storage

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    domains: ['ndbmjgzhowixqwyjooid.supabase.co'], // Add your Supabase domain
     remotePatterns: [
       {
         protocol: "https",
